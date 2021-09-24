759f14126294fe4121178a761958db231ff6653f
hashmimdfaiz <hashmimdfaiz077@gmail.com>
2021-09-24 17:22:52 +0530

    host

--- proj_cus/settings.py
+++ proj_cus/settings.py
@@ -126,9 +126,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 # STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(STATIC_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(, 'static'),
+# )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
