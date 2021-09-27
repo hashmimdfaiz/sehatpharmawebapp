bf0c0f84308798b8c434391ed062acbdfa2359fa
hashmimdfaiz <hashmimdfaiz077@gmail.com>
2021-09-27 20:46:32 +0530

    Initial commit

--- proj_cus/settings.py
+++ proj_cus/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
