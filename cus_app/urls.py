775640dc74ae950baa15e96fe45b7b7afda8b25b
hashmimdfaiz <hashmimdfaiz077@gmail.com>
2021-09-27 18:56:36 +0530

    Initial commit

--- cus_app/urls.py
+++ cus_app/urls.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 def okay(request):
     return HttpResponse('pretend-binary-data-here', content_type='image/jpeg')
 urlpatterns = [
-    path('',Index.as_view(),name='homepage'),
+    path('' or '/',Index.as_view(),name='homepage'),
     path('sub',sub_page,name='submit_page'),
     path('favicon.ico', okay),
 
