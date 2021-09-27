777751ff89a0bcfab658fcd6629e1c1b3ebf4975
hashmimdfaiz <hashmimdfaiz077@gmail.com>
2021-09-27 19:50:37 +0530

    Initial commit

--- cus_app/urls.py
+++ cus_app/urls.py
@@ -5,7 +5,7 @@ from .views import Index, sub_page
 from django.http import HttpResponse
 
 def okay(request):
-    return HttpResponse('pretend-binary-data-here', content_type='image/jpeg')
+    return HttpResponse('pretend-binary-data-here')
 urlpatterns = [
     path('',Index.as_view(),name='homepage'),
     path('sub',sub_page,name='submit_page'),
